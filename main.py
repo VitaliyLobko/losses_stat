1f0df8f0e89e059db855a781cb7d45d375902e13
Vitaliy Lobko <vitaliy.lobko@gmail.com>
2024-11-04 10:34:38 +0200

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+import re
+from datetime import datetime
+import json
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://index.minfin.com.ua/ua/russian-invading/casualties/"
+
+
+def get_url():
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    content = soup.select("div[class=ajaxmonth] h4[class=normal] a")
+    urls = ["/"]
+    prefix = "/month.php?month="
+    for tag_a in content:
+        urls.append(prefix + re.search(r"\d{4}-\d{2}", tag_a["href"]).group())
+    return urls
+
+
+def spider(urls):
+    data = []
+    for url in urls:
+        response = requests.get(base_url + url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        content = soup.select("ul[class=see-also] li[class=gold]")
+        for element in content:
+            result = {}
+            date = element.find("span", attrs={"class": "black"}).text
+            try:
+                date = datetime.strptime(date, "%d.%m.%Y").isoformat()
+            except ValueError:
+                continue
+
+            result.update({"date": date})
+            losses = element.find("div").find("div").find("ul")
+            for l in losses:
+                title, quantity, *rest = l.text.split("—")
+                title = title.strip()
+                quantity = re.search(r"\d+", quantity).group()
+                result.update({title: quantity})
+        data.append(result)
+
+    return data
+
+
+if __name__ == "__main__":
+    urls_for_parser = get_url()
+    r = spider(urls_for_parser)
+    with open("losses.json", "w", encoding="utf-8") as fd:
+        json.dump(r, fd, ensure_ascii=False)
